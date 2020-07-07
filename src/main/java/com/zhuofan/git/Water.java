468b28d6f06267cf6a1e16821462e011706e3725
thinkpad <thinkpad@qq.com>
2020-07-07 18:00:51 +0800

    water文件添加字段07071803

--- src/main/java/com/zhuofan/git/Water.java
+++ src/main/java/com/zhuofan/git/Water.java
@@ -1,5 +1,22 @@
 package com.zhuofan.git;
 
 public class Water {
-
+	
+	private String name;
+	private String quality;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	
+	
 }
